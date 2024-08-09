08c02a65be1801dd95d002eecfc060e55e5b0491
katya havriuk <0303gavkat@gmail.com>
2024-08-09 15:17:46 -0600

    web sockets

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import './App.css';
 import { MessageForm } from './MessageForm.jsx';
 import { MessageList } from './MessageList.jsx';
 
 const DataLoader = ({ onData }) => {
-  const loadData = async () => {
-    const response = await axios.get('http://localhost:3005/messages');
-    onData(response.data);
-    loadData();
-  };
-
   useEffect(() => {
-    const evtSource = new EventSource('http://localhost:3005/messages');
+    const socket = new WebSocket('ws://localhost:3005/messages');
 
-    evtSource.onmessage = (e) => {
-      onData(JSON.parse(e.data));
-    };
+    socket.addEventListener('message', (event) => {
+      const text = JSON.parse(event.data);
+      onData(text);
+    });
+
     return () => {
-      evtSource.close()
-    }
+      socket.close();
+    };
   }, []);
 
   return <h1 className='title'>Chat application</h1>;
 };
 
+export default DataLoader;
+
 export function App() {
   const [messages, setMessages] = useState([]);
 
   function saveData(data) {
-    setMessages((messages) => [data, ...messages]);
+    setMessages((prevMessages) => [data, ...prevMessages]);
   }
 
   return (
